c96c8553d4d59c9a026d574dee6b3a8f025af5e1
andrewyk99 <andrewyk99@hotmail.com>
2022-08-04 17:01:23 -0600

    turned portfolio section to dynamically create through an array

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,51 @@ import IMG4 from '../../assets/images/portfolio4.jpg';
 import IMG5 from '../../assets/images/portfolio5.png';
 import IMG6 from '../../assets/images/portfolio6.jpg';
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Project 1',
+    github: 'https://github.com/',
+    demo: 'https://liveapplication.com'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Project 2',
+    github: 'https://github.com/',
+    demo: 'https://liveapplication.com'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Project 3',
+    github: 'https://github.com/',
+    demo: 'https://liveapplication.com'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Project 4',
+    github: 'https://github.com/',
+    demo: 'https://liveapplication.com'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Project 1',
+    github: 'https://github.com/',
+    demo: 'https://liveapplication.com'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Project 6',
+    github: 'https://github.com/',
+    demo: 'https://liveapplication.com'
+  },
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -14,77 +59,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {/* Project 1 */}
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Title 1" />
-          </div>
-            <h3>This is a portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a href="http://github.com/" className='btn' target='_blank' rel="noopener noreferrer">GitHub</a>
-              <a href="http://website" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
-            </div>
-        </article>
-
-        {/* Project 2 */}
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Title 1" />
-          </div>
-            <h3>This is a portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a href="http://github.com/" className='btn' target='_blank' rel="noopener noreferrer">GitHub</a>
-              <a href="http://website" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
-            </div>
-        </article>
-
-        {/* Project 3 */}
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Title 1" />
-          </div>
-            <h3>This is a portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a href="http://github.com/" className='btn' target='_blank' rel="noopener noreferrer">GitHub</a>
-              <a href="http://website" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
-            </div>
-        </article>
-
-        {/* Project 4 */}
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Title 1" />
-          </div>
-            <h3>This is a portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a href="http://github.com/" className='btn' target='_blank' rel="noopener noreferrer">GitHub</a>
-              <a href="http://website" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
-            </div>
-        </article>
-
-        {/* Project 5 */}
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Title 1" />
-          </div>
-            <h3>This is a portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a href="http://github.com/" className='btn' target='_blank' rel="noopener noreferrer">GitHub</a>
-              <a href="http://website" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
-            </div>
-        </article>
-
-        {/* Project 6 */}
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="Title 1" />
-          </div>
-            <h3>This is a portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a href="http://github.com/" className='btn' target='_blank' rel="noopener noreferrer">GitHub</a>
-              <a href="http://website" className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
-            </div>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank' rel="noopener noreferrer">GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel="noopener noreferrer">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
