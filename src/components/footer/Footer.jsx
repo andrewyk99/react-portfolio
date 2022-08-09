b5385ff07aa66331ff1dba9c2110a1f8abf180de
andrewyk99 <andrewyk99@hotmail.com>
2022-08-08 20:27:45 -0600

    created and styled footer and contact components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './footer.css';
+import {BsLinkedin} from 'react-icons/bs';
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>ANDREW</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://github.com/andrewyk99" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/andrew-kim-40b25722b/" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Andrew Kim. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
