270c7b34fff080c8a5ed78adc9a9e08cf8d3cad2
andrewyk99 <andrewyk99@hotmail.com>
2022-08-23 19:35:50 -0600

    styling tweaks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import {FaGithub} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>ANDREW</a>
+      <a href="#" className='footer__logo'>ANDREW KIM</a>
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
